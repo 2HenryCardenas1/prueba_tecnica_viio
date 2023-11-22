38cbd12f693478e3504a7b472397bf3b6fbb8624
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-21 20:05:32 -0500

    Add Navbar component and linksPages utility

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  IconHeart,
+  IconHome,
+  IconLogin,
+  IconSearch,
+  IconShoppingCart,
+} from "@tabler/icons-react";
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+export default function Navbar() {
+  const pathname = usePathname();
+
+  const IconLink = ({ href, icon, text }) => {
+    return (
+      <Link
+        href={href}
+        className={clsx(
+          "flex flex-row justify-center items-center",
+          pathname === href &&
+            "text-black font-semibold bg-white px-2 py-1 rounded-full gap-2"
+        )}
+      >
+        {icon}
+        {pathname === href && (
+          <span className="hidden sm:block font-semibold">{text}</span>
+        )}
+      </Link>
+    );
+  };
+
+  return (
+    <div className=" flex justify-between p-5 bg-slate-800 items-center">
+      <div>
+        <Link href="/">
+          <h1 className="text-2xl font-bold">MAYNOOT</h1>
+        </Link>
+      </div>
+      <div className="flex justify-between gap-2 sm:gap-3 md:gap-5">
+        <IconLink href="/" icon={<IconHome size={20} />} text="Home" />
+        <IconLink
+          href="/search"
+          icon={<IconSearch size={20} />}
+          text="Search"
+        />
+        <IconLink
+          href="/cart"
+          icon={<IconShoppingCart size={20} />}
+          text="Cart"
+        />
+        <IconLink
+          href="/favorites"
+          icon={<IconHeart size={20} />}
+          text="Favorites"
+        />
+        <IconLink href="/login" icon={<IconLogin size={20} />} text="Login" />
+      </div>
+    </div>
+  );
+}
---
+++ src/utils/linksPages.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Login",
+    path: "/login",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+];
+
+export { links };
