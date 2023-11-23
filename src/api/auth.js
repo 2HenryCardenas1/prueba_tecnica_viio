ae9b2e123ffae78aa686b2d4d9dcb772469071eb
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-22 21:34:38 -0500

    Add registerFetch function to handle user registration

--- src/api/auth.js
+++ src/api/auth.js
@@ -18,4 +18,24 @@ const loginFetch = async ({ username, password }) => {
     }
   }
 };
-export { loginFetch };
+
+const registerFetch = async ({ firstName, lastName, username, password }) => {
+  try {
+    const response = await axios.post("https://dummyjson.com/users/add", {
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      password: password,
+    });
+
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.status === 400) {
+      return error.response.data;
+    } else {
+      throw new Error(ERROR_PROBLEMA_SOLICITUD + error.message);
+    }
+  }
+};
+
+export { loginFetch, registerFetch };
