4409c78865881c0c72c2295197cb535447587eaf
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-22 21:35:03 -0500

    Add register functionality to auth store

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,8 +1,9 @@
-import { loginFetch } from "@/api/auth";
+import { loginFetch, registerFetch } from "@/api/auth";
 import { create } from "zustand";
 
 const useStore = create((set) => ({
   isAuthenticated: false,
+  registered: false,
   messageError: "",
   login: async ({ username, password }) => {
     const res = await loginFetch({ username, password });
@@ -16,6 +17,22 @@ const useStore = create((set) => ({
   },
 
   logout: () => set({ isAuthenticated: false }),
+
+  register: async ({ firstName, lastName, username, password }) => {
+    const res = await registerFetch({
+      firstName,
+      lastName,
+      username,
+      password,
+    });
+
+    if (res) {
+      set({ registered: true });
+    } else {
+      set({ registered: false });
+      set({ messageError: res.message });
+    }
+  },
 }));
 
 export default useStore;
