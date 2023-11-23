961fd8a538466749e3e9a608460c5228b744adfb
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-23 00:04:15 -0500

    Refactor search page layout

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,9 +1,17 @@
 import PrivateRoute from "@/components/PrivateRoute";
+import SearchProducts from "@/components/SearchProducts";
 
 export default function SearchPage() {
   return (
     <PrivateRoute>
-      <div>SearchPage</div>;
+      <main
+        className="flex min-h-screen flex-col items-center  px-5 py-10 gap-5
+    
+      md:px-10 md:py-20 lg:px-20 lg:py-20 xl:px-40 xl:py-20 2xl:px-80 2xl:py-20
+    "
+      >
+        <SearchProducts />
+      </main>
     </PrivateRoute>
   );
 }
