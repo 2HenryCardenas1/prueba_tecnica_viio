39460fd8948cac2ba29ff6ef1767469cc039975f
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-22 21:52:38 -0500

    Add PrivateRoute component for authenticated routes

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+"use client";
+import useStore from "@/store/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const PrivateRoute = ({ children }) => {
+  const router = useRouter();
+  const { isAuthenticated } = useStore((state) => state);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push("/auth/login");
+    }
+  }, [isAuthenticated, router]);
+
+  return <>{children}</>;
+};
+
+export default PrivateRoute;
