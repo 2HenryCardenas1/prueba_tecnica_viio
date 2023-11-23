7accfddfec2f3fac9975bd2c948dd5f2f6fc21bc
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-23 10:01:54 -0500

    Refactor authentication store with Zustand middleware

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,38 +1,64 @@
 import { loginFetch, registerFetch } from "@/api/auth";
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
-const useStore = create((set) => ({
-  isAuthenticated: false,
-  registered: false,
-  messageError: "",
-  login: async ({ username, password }) => {
-    const res = await loginFetch({ username, password });
+const useStore = create(
+  persist(
+    devtools((set) => ({
+      isAuthenticated: false,
+      registered: false,
+      messageError: "",
+      login: async ({ username, password }) => {
+        try {
+          const res = await loginFetch({ username, password });
 
-    if (res.token) {
-      set({ isAuthenticated: true });
-    } else {
-      set({ isAuthenticated: false });
-      set({ messageError: res.message });
+          if (res.token) {
+            set({ isAuthenticated: true });
+            setToken(res.token);
+          } else {
+            set({ isAuthenticated: false, error: res.message });
+          }
+        } catch (error) {
+          set({ isAuthenticated: false, error: "Error during login" });
+        }
+      },
+
+      logout: () => {
+        set({ isAuthenticated: false });
+        clearToken();
+      },
+
+      register: async ({ firstName, lastName, username, password }) => {
+        try {
+          const res = await registerFetch({
+            firstName,
+            lastName,
+            username,
+            password,
+          });
+
+          if (res) {
+            set({ registered: true });
+          } else {
+            set({ registered: false, error: res.message });
+          }
+        } catch (error) {
+          set({ registered: false, error: "Error during registration" });
+        }
+      },
+    })),
+    {
+      name: "auth-storage",
     }
-  },
+  )
+);
 
-  logout: () => set({ isAuthenticated: false }),
+const setToken = (token) => {
+  localStorage.setItem("token", token);
+};
 
-  register: async ({ firstName, lastName, username, password }) => {
-    const res = await registerFetch({
-      firstName,
-      lastName,
-      username,
-      password,
-    });
-
-    if (res) {
-      set({ registered: true });
-    } else {
-      set({ registered: false });
-      set({ messageError: res.message });
-    }
-  },
-}));
+const clearToken = () => {
+  localStorage.removeItem("token");
+};
 
 export default useStore;
