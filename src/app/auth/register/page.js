1c0884faf86b297bd29b0bb3da7938e2d8e4609f
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-22 21:35:23 -0500

    Add Register component to RegisterPage

--- src/app/auth/register/page.js
+++ src/app/auth/register/page.js
@@ -1,3 +1,14 @@
+import Register from "@/components/Register";
+
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <main
+      className="text-center flex min-h-screen flex-col items-center  px-5 py-10 gap-5
+    
+      md:px-10 md:py-20 lg:px-20 lg:py-20 xl:px-40 xl:py-20 2xl:px-80 2xl:py-20"
+    >
+      <h1 className="text-2xl font-bold mb-10">MAYNOOTH</h1>
+      <Register />
+    </main>
+  );
 }
---
+++ src/components/Register.jsx
@@ -0,0 +1,229 @@
+"use client";
+
+import useStore from "@/store/auth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    watch,
+  } = useForm();
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+  const [passwordMatch, setPasswordMatch] = useState(true);
+
+  const {
+    register: reguisterStore,
+    messageError,
+    registered,
+  } = useStore((state) => state);
+
+  const handleRegsiter = (data) => {
+    setLoading(true);
+
+    reguisterStore({
+      firstName: data.firstName,
+      lastName: data.lastName,
+      username: data.username,
+      password: data.password,
+    });
+
+    setLoading(false);
+  };
+
+  const password = watch("password", "");
+  const repeatPassword = watch("repeatPassword", "");
+
+  useEffect(() => {
+    if (registered) {
+      router.refresh();
+      router.push("/auth/login");
+    }
+  }, [registered, router]);
+
+  useEffect(() => {
+    setPasswordMatch(password === repeatPassword);
+  }, [password, repeatPassword]);
+
+  return (
+    <div className="border p-5 w-full md:w-[600px] shadow-md rounded-md flex flex-col gap-5 ">
+      <form onSubmit={handleSubmit(handleRegsiter)}>
+        <div className="flex justify-between items-center gap-1 ">
+          <div className="flex flex-col text-left  flex-1">
+            <label htmlFor="firstName" className="text-left font-bold p-1">
+              First name
+            </label>
+
+            <input
+              className=" form-input border-2 border-black text-left p-2 rounded-md"
+              type="text"
+              name="firstName"
+              id="firstName"
+              placeholder="Enter your first name"
+              {...register("firstName", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+                minLength: {
+                  value: 8,
+                  message: "First name should not be less than 8 characters",
+                },
+              })}
+            />
+            {errors.firstName?.message && (
+              <span className="text-red-500 ">{errors.firstName?.message}</span>
+            )}
+          </div>
+
+          <div className="flex flex-col text-left flex-1">
+            <label htmlFor="lastName" className="text-left font-bold p-1">
+              Last name
+            </label>
+
+            <input
+              className=" form-input border-2 border-black text-left p-2 rounded-md"
+              type="text"
+              name="lastName"
+              id="lastName"
+              placeholder="Enter your last name"
+              {...register("lastName", {
+                required: {
+                  value: true,
+                  message: "This field is required",
+                },
+              })}
+            />
+            {errors.lastName?.message && (
+              <span className="text-red-500 ">{errors.lastName?.message}</span>
+            )}
+          </div>
+        </div>
+
+        <div className="flex flex-col text-left">
+          <label htmlFor="username" className="text-left font-bold p-1">
+            Username
+          </label>
+
+          <input
+            className=" form-input border-2 border-black text-left p-2 rounded-md"
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter your username"
+            {...register("username", {
+              required: {
+                value: true,
+                message: "This field is required",
+              },
+            })}
+          />
+          {errors.username?.message && (
+            <span className="text-red-500 ">{errors.username?.message}</span>
+          )}
+        </div>
+
+        <div className="flex flex-col text-left">
+          <label htmlFor="password" className="text-left font-bold p-1">
+            Password
+          </label>
+          <input
+            className="border-2 border-black text-left p-2 rounded-md"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Your password"
+            {...register("password", {
+              required: {
+                value: true,
+                message: "This field is required",
+              },
+              maxLength: {
+                value: 20,
+                message: "Password should not be more than 20 characters",
+              },
+              minLength: {
+                value: 8,
+                message: "Password should not be less than 8 characters",
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                message:
+                  "Password should contain at least one uppercase letter, one lowercase letter and one number",
+              },
+            })}
+          />
+
+          {errors.password?.message && (
+            <span className="text-red-500 ">{errors.password?.message}</span>
+          )}
+        </div>
+
+        <div className="flex flex-col text-left">
+          <label htmlFor="repeatPassword" className="text-left font-bold p-1">
+            Repeat Password
+          </label>
+          <input
+            className={`border-2 border-black text-left p-2 rounded-md ${
+              passwordMatch ? "" : "border-red-500"
+            }`}
+            type="password"
+            name="repeatPassword"
+            id="repeatPassword"
+            placeholder="Repeat your password"
+            {...register("repeatPassword", {
+              required: {
+                value: true,
+                message: "This field is required",
+              },
+              validate: (value) =>
+                value === password || "Passwords do not match",
+            })}
+          />
+
+          {errors.repeatPassword?.message && (
+            <span className="text-red-500 ">
+              {errors.repeatPassword?.message}
+            </span>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className={` p-2 mt-5 rounded-md w-full font-semibold ${
+            isValid ? "bg-black text-white" : "border-2 border-black"
+          }`}
+        >
+          Log in {loading && <span className="animate-spin">🌀</span>}
+        </button>
+      </form>
+
+      {messageError && (
+        <div className="text-red-500 text-center">{messageError}</div>
+      )}
+      <div className="flex justify-center flex-col gap-3">
+        <strong className="text-center text-sm mb-2">or</strong>
+
+        <button className=" text-black border-2 border-black p-2 rounded-md w-full font-semibold">
+          Continue with Google
+        </button>
+        <button className=" text-black border-2 border-black p-2 rounded-md w-full font-semibold">
+          Continue with Facebook
+        </button>
+
+        <p>
+          You have an account?{" "}
+          <Link href="/auth/login">
+            <strong>Login</strong>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+}
