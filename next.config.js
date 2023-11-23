3f15cd91d9881b45c91adc1a39863eabbe9c3ff8
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-23 00:05:12 -0500

    Add image domains and update package dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "localhost",
+      "images.ctfassets.net",
+      "www.oyorooms.com",
+      "www.tusktravel.com",
+      "upload.wikimedia.org",
+      "cdn.britannica.com",
+      "static2.tripoto.com",
+      "i.natgeofe.com",
+      "picsum.photos",
+      "i.dummyjson.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
