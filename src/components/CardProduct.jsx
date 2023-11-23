d55a31f39ea2db640d86d30fec4f24d66eb0fed7
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-22 23:43:51 -0500

    Refactor CardProduct component to use dynamic data

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -13,29 +13,27 @@
 import { IconHeart } from "@tabler/icons-react";
 import Image from "next/image";
 
-export default function CardProduct() {
+export default function CardProduct({ product }) {
   return (
-    <div className="max-w-[300px]   rounded-lg shadow bg-slate-800 mt-5">
-      <div className="relative rounded-lg ">
+    <div className="bg-white rounded shadow overflow-hidden">
+      <div className="relative rounded-lg h-54">
         <div className="absolute top-0 right-0 p-2 ">
           <IconHeart size={30} color="red" />
         </div>
 
         <Image
-          className="rounded-t-lg object-contain w-full h-full z-10"
-          src="https://picsum.photos/500/300/?image=10"
-          alt=""
+          src={product.thumbnail}
+          alt={product.title}
+          className="h-56 w-full object-cover"
           width={500}
           height={300}
         />
       </div>
 
       <div className="p-5">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-          Producto 1
-        </h5>
+        <h5 className="font-semibold text-lg">{product.title}</h5>
 
-        <p className="mb-3 font-semibold text-white ">Rp. 100.000</p>
+        <p className="mb-3 font-semibold text-gray-500 ">${product.price}</p>
       </div>
     </div>
   );
