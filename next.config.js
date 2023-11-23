398768f4c7f8e00a5036e55a4ac1d0fd8d4e0b52
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-11-23 10:01:43 -0500

    Update next.config.js to allow remote image patterns

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "images.ctfassets.net",
-      "www.oyorooms.com",
-      "www.tusktravel.com",
-      "upload.wikimedia.org",
-      "cdn.britannica.com",
-      "static2.tripoto.com",
-      "i.natgeofe.com",
-      "picsum.photos",
-      "i.dummyjson.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
